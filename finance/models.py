ff73a02a80b77f1e8612170bb7707e91aafc1688
M-Pidlisnyi <mishapropatriot@gmail.com>
2025-03-26 20:48:13 +0200

    minor models fix

--- finance/models.py
+++ finance/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 # Create your models here.
@@ -17,8 +18,11 @@ class FinanceAccount(models.Model):
    
 
 class DebitAccount(FinanceAccount):
-    default_commission = models.DecimalField(decimal_places=2, max_digits=4, verbose_name="default commission (%)")
-    cashback_rate= models.DecimalField(decimal_places=2, max_digits=4, verbose_name="default cashback (%)")
+    default_commission = models.DecimalField(decimal_places=2, max_digits=4, default=0.35,
+                                             validators=[MinValueValidator(0.01)],
+                                             verbose_name="default commission (%)",help_text="base(but not only) commission for each transaction")
+    cashback_rate= models.DecimalField(decimal_places=2, max_digits=4, default=1.35,
+                                       verbose_name="default cashback (%)", help_text="this cashback will be received from some categories of transaction")
     accumulated_cashback = models.DecimalField(decimal_places=2, max_digits=10, default=0)
 
 class CreditAccount(FinanceAccount):
